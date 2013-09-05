bedf12576cda2c77f9a98f3d13148e69cfbb3f55
Shane M. Ross <scross23@gmail.com>
2013-09-05 16:28:31 -0500

    using Foundation now and changed some fonts and styles

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$("#motion").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#motiondesign").offset().top
+    }, 2000);
+});
+
+$("#insta").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#instagram").offset().top
+    }, 2000);
+});
